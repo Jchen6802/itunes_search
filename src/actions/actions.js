c345461b002493dfc0201a33d7482559373b33c8
John Chen <jchen6802@gmail.com>
2018-10-04 09:31:39 -0400

    Empty previous results during every search

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -12,14 +12,20 @@ export const updateSearchTerm = (value) => {
 export const fetchData = (searchTerm) => {
     return (dispatch, getState) => {
         const itunesApiUrl = `${Constants.FETCH_SOURCE}?term=${searchTerm}&entity=album&attribute=artistTerm`;
+
+        dispatch(setAlbums(Constants.INITIAL_STATE.albums));
         return axios.get(itunesApiUrl)
             .then(response => {
-                dispatch({
-                    type: Constants.SET_ALBUMS,
-                    value: response.data.results
-                });
+                dispatch(setAlbums(response.data.results));
             }).catch( error => {
                 console.log(error);
             });
     };
+};
+
+const setAlbums = (value) => {
+    return {
+        type: Constants.SET_ALBUMS,
+        value
+    }
 };
